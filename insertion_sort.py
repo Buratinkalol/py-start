23e669c463edd88d288d4e170be32cb26b1baa26
Buratinkalol <h12vbn6@gmail.com>
2017-12-18 08:59:36 -0800

    insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,5 +1,13 @@
 def insertion_sort():
-    print("hello")
+    array = [8, 4, 5, 1, 3, 6]
+    for j in range(1, len(array)):
+        i = j - 1
+        key = array[j]
+        while i >= 0 and array[i] > key:
+            array[i+1] = array[i]
+            i = i - 1
+        array[i+1] = key
+    print(array)
     return 0
 
 
